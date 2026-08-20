a3e29a957419cd8fdb0d3f9ea4cf554290be152a
adamova47 <125302289+adamova47@users.noreply.github.com>
2026-08-20 21:20:05 +0200

    Create a simple main to run the code from

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
-from app.weather import run_daily_weather_update
+from app.config import LOCATION_ID, LATITUDE, LONGITUDE
+from app.weather import run_daily_weather_update, get_weather_data
+from app.observations import run_observations_update
 
 
 if __name__ == "__main__":
     try:
-        run_daily_weather_update()
-        print("Daily weather update completed successfully.")
+        print(get_weather_data(LATITUDE, LONGITUDE))
+        # run_observations_update()
     except Exception as e:
-        print(f"Error running daily weather update: {e}")
+        print(f"Error: {e}")
         sys.exit(1)
