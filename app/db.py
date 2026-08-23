556942602537958a02416eb809f0ab7ee5d2fcb6
adamova47 <125302289+adamova47@users.noreply.github.com>
2026-08-23 22:34:03 +0200

    Renamed app folder to backend for future work

--- app/db.py
+++
@@ -1,6 +0,0 @@
-import psycopg
-from app.config import DATABASE_URL
-
-
-def get_connection():
-    return psycopg.connect(DATABASE_URL)
--- app/locations.py
+++
@@ -1,4 +0,0 @@
-def get_locations(cursor):
-    cursor.execute("select id, latitude, longitude from locations;")
-    columns = ["id", "latitude", "longitude"]
-    return [dict(zip(columns, row)) for row in cursor.fetchall()]
